d1c506ff6dc36f755e47c6c85f281d0dd4901a04
Kapil <kapilintern@gmail.com>
2016-01-19 01:23:00 +0530

    use of ng-show to hide and show menu item

--- gulpfile.js
+++ gulpfile.js
@@ -14,26 +14,12 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     del = require('del');
 
-gulp.task('jshint', function() {
-  return gulp.src('app/scripts/**/*.js')
-  .pipe(jshint())
-  .pipe(jshint.reporter(stylish));
-});
-gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
-      .pipe(usemin({
-        css:[minifycss(),rev()],
-        js: [uglify(),rev()]
-      }))
-      .pipe(gulp.dest('dist/'));
-});
+var ngannotate = require('gulp-ng-annotate');
 
-// Images
-gulp.task('imagemin', function() {
-  return del(['dist/images']), gulp.src('app/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images'))
-    .pipe(notify({ message: 'Images task complete' }));
+gulp.task('jshint', function() {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
 });
 
 // Clean
@@ -41,42 +27,68 @@ gulp.task('clean', function() {
     return del(['dist']);
 });
 
-gulp.task('copyfonts', ['clean'], function() {
-   gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-   .pipe(gulp.dest('./dist/fonts'));
-   gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
-   .pipe(gulp.dest('./dist/fonts'));
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('usemin', 'imagemin','copyfonts');
 });
+
+gulp.task('usemin',['jshint'], function () {
+    return gulp.src('./app/menu.html')
+        .pipe(usemin({
+            css:[minifycss(),rev()],
+            js: [uglify(),rev()]
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+    return del(['dist/images']), gulp.src('app/images/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('copyfonts', ['clean'], function() {
+    gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
 // Watch
 gulp.task('watch', ['browser-sync'], function() {
-  // Watch .js files
-  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
-      // Watch image files
-  gulp.watch('app/images/**/*', ['imagemin']);
+    // Watch .js files
+    gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
+    // Watch image files
+    gulp.watch('app/images/**/*', ['imagemin']);
 
 });
 
 gulp.task('browser-sync', ['default'], function () {
-   var files = [
-      'app/**/*.html',
-      'app/styles/**/*.css',
-      'app/images/**/*.png',
-      'app/scripts/**/*.js',
-      'dist/**/*'
-   ];
+    var files = [
+        'app/**/*.html',
+        'app/styles/**/*.css',
+        'app/images/**/*.png',
+        'app/scripts/**/*.js',
+        'dist/**/*'
+    ];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: "dist",
-         index: "menu.html"
-      }
-   });
-        // Watch any files in dist/, reload on change
-  gulp.watch(['dist/**']).on('change', browserSync.reload);
+    browserSync.init(files, {
+        server: {
+            baseDir: "dist",
+            index: "menu.html"
+        }
     });
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', browserSync.reload);
+});
 
-
-// Default task
-gulp.task('default', ['clean'], function() {
-    gulp.start('usemin', 'imagemin','copyfonts');
-}); 
+gulp.task('usemin',['jshint'], function () {
+    return gulp.src('./app/menu.html')
+        .pipe(usemin({
+            css:[minifycss(),rev()],
+            js: [ngannotate(),uglify(),rev()]
+        }))
+        .pipe(gulp.dest('dist/'));
+});
